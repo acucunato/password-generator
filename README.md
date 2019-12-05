5b235f7bc5da0a6757096d94ca7f75e0ca9126cc
Alyssa Cucunato <acucunato18@gmail.com>
2019-12-04 19:24:30 -0500

    final commit, finished readme, used prettier for code formatting

--- README.md
+++ README.md
@@ -1,40 +1,35 @@
 # Password Generator
 
-Created during Week 4 of UPenn Coding Bootcamp.
+Created during Week 3 of UPenn Coding Bootcamp.
 
 ## Overview
+
 This application will generate a random password based on user-selected criteria. User will be prompted to choose a desired length and types of characters to include in the password. Characters in the password will then be randomly chosen in a random order based on user selection and desired length.
 
 ## Utilizing the App
 
-* View the app: [Here](https://acucunato.github.io/password-generator/ "Here") 
+- View the app: [Here](https://acucunato.github.io/password-generator/ "Here")
 
-* To start, click on "Generate Password" button.
+- To start, click on "Generate Password" button.
 
-* User will answer prompts as desired.
-    * User must select a number between 8-128 characters.
-    * User must confirm at least one of the valid inputs (special, number, upper & lower characters) to continue. 
+- User will answer prompts as desired.
 
-* After password is generated, user can copy password to clipboard by clicking the "Copy to Clipboard" button.
+  - User must select a number between 8-128 characters.
+  - User must confirm at least one of the valid inputs (special, number, upper & lower characters) to continue.
+
+- After password is generated, user can copy password to clipboard by clicking the "Copy to Clipboard" button.
 
 ## Demo
 
 ![Password Generator Image](assets/images/password-generator-photo.png "Password Generator")
 
+## Tech used
 
-## Tech used 
-
-* HTML
-* CSS
-* Bootstrap
-* Javascript
+- HTML
+- CSS
+- Bootstrap
+- Javascript
 
 ## Contributers
 
-* Alyssa Cucunato
-
-
-
-
-
-
+- Alyssa Cucunato
--- assets/javascript/generator.js
+++ assets/javascript/generator.js
@@ -1,94 +1,101 @@
-
-// Generate password - if password is not between 8-128 char message will alert telling user to input a valid length. 
+// Generate password - if password is not between 8-128 char message will alert telling user to input a valid length.
 
 function generatePassword() {
-    var finalLength = prompt("How many characters would you like your password to contain? (between 8-128 characters)");
+  var finalLength = prompt(
+    "How many characters would you like your password to contain? (between 8-128 characters)"
+  );
 
-    if (finalLength < 8 || finalLength > 128) {
-        alert("Invalid length, please enter a number between 8-128 characters!")
-        return;
-    }
-    
-    //confirm which char are being used in the password
-    var confirmSpecial = confirm("Click OK to confirm including special characters.");
-    var confirmNumber = confirm("Click OK to confirm including numeric characters.");
-    var confirmLower = confirm("Click OK to confirm including lowercase characters.");
-    var confirmUpper = confirm("Click OK to confirm including uppercase characters.");
+  if (finalLength < 8 || finalLength > 128) {
+    alert("Invalid length, please enter a number between 8-128 characters!");
+    return;
+  }
 
-    // if all of these are false user cannot continue (! means false)
-    if (!confirmSpecial && !confirmNumber && !confirmUpper && !confirmLower) {
-        return alert("Must choose at least one character type. Try again!");
-    }
+  //confirm which char are being used in the password
+  var confirmSpecial = confirm(
+    "Click OK to confirm including special characters."
+  );
+  var confirmNumber = confirm(
+    "Click OK to confirm including numeric characters."
+  );
+  var confirmLower = confirm(
+    "Click OK to confirm including lowercase characters."
+  );
+  var confirmUpper = confirm(
+    "Click OK to confirm including uppercase characters."
+  );
 
-    // define password var to hold password data
-    var password = "";
-    
-    // generates random char in password string using the user inputted length, if true adds char to empty password string
-    for (i = 0; i < finalLength; i++) {
+  // if all of these are false user cannot continue (! means false)
+  if (!confirmSpecial && !confirmNumber && !confirmUpper && !confirmLower) {
+    return alert("Must choose at least one character type. Try again!");
+  }
 
-        if (confirmSpecial && password.length < finalLength) {
-            password += specialChar();
-        }
+  // define password var to hold password data
+  var password = "";
 
-        if (confirmNumber && password.length < finalLength) {
-            password += numberChar();
-        }
-
-        if (confirmUpper && password.length < finalLength) {
-            password += upperChar();
-        }
-
-        if (confirmLower && password.length < finalLength) {
-            password += lowChar();
-        }
+  // generates random char in password string using the user inputted length, if true adds char to empty password string
+  for (i = 0; i < finalLength; i++) {
+    if (confirmSpecial && password.length < finalLength) {
+      password += specialChar();
     }
 
-    //turned "password" into an array in order to hold password data and to be able to randomize char
-    var passwordArr = password.split("");
+    if (confirmNumber && password.length < finalLength) {
+      password += numberChar();
+    }
 
-    //randomizing order of the sort & joining together
-    passwordArr.sort(function() {
-        return 0.5 - Math.random();
-    });
+    if (confirmUpper && password.length < finalLength) {
+      password += upperChar();
+    }
 
-    // final password generated!!! :)
-    password = passwordArr.join("");
+    if (confirmLower && password.length < finalLength) {
+      password += lowChar();
+    }
+  }
 
-    // final generated password in an alert
-    alert("Your password is " + password);
+  //turned "password" into an array in order to hold password data and to be able to randomize char
+  var passwordArr = password.split("");
 
+  //randomizing order of the sort & joining together
+  passwordArr.sort(function() {
+    return 0.5 - Math.random();
+  });
 
-    //generates password to text area
-    document.getElementById("generated").value = password;
+  // final password generated!!! :)
+  password = passwordArr.join("");
+
+  // final generated password in an alert
+  alert("Your password is " + password);
+
+  //generates password to text area
+  document.getElementById("generated").value = password;
 }
 
 //copy to clipboard button
 
 function copyPassword() {
-    document.getElementById("generated").select();
+  document.getElementById("generated").select();
 
-    document.execCommand("Copy");
+  document.execCommand("Copy");
 
-    alert("Password copied to clipboard.");    
+  alert("Password copied to clipboard.");
 }
 
-// generates random char to be used in the password 
+// generates random char to be used in the password
 function specialChar() {
-    var special = "!#$%&'()*+,-./:;<=>?@]/[^_`{|}~";
-    return special[Math.floor(Math.random() * special.length)]
+  var special = "!#$%&'()*+,-./:;<=>?@]/[^_`{|}~";
+  return special[Math.floor(Math.random() * special.length)];
 }
 
 function numberChar() {
-    var number = "0123456789";
-    return number[Math.floor(Math.random() * number.length)]
+  var number = "0123456789";
+  return number[Math.floor(Math.random() * number.length)];
 }
 
 function lowChar() {
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    return lower[Math.floor(Math.random() * lower.length)]
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  return lower[Math.floor(Math.random() * lower.length)];
 }
 
 function upperChar() {
-    var upper = "ABCDEFGHIJKLMNOPQRZTUVWXYZ";
-    return upper[Math.floor(Math.random() * upper.length)]
+  var upper = "ABCDEFGHIJKLMNOPQRZTUVWXYZ";
+  return upper[Math.floor(Math.random() * upper.length)];
 }
